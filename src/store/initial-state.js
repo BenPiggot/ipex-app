a819ffd0d443e773ea5541be1455947ab8a80526
ben <wbpiggot@gmail.com>
2017-12-29 17:19:15 -0800

    More work building up data

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -26,7 +26,11 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["98e2b39c-bbad-40a2-b3b2-60919e9e6c2c", "dd55f78a-1264-4518-84d7-a8534fe354aa", "6fa98c28-0892-40ae-afab-531a39249fc0"],
+          "filters": [
+                      "98e2b39c-bbad-40a2-b3b2-60919e9e6c2c", 
+                      "dd55f78a-1264-4518-84d7-a8534fe354aa", 
+                      "6fa98c28-0892-40ae-afab-531a39249fc0"
+                    ],
           "id": "98e2b39c-bbad-40a2-b3b2-60919e9e6c2c",
           "name": "FPC/FFC"
         },
@@ -40,14 +44,28 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["a099a33a-c5da-48bc-a733-8ece97efbf8a", "abc4fdb2-e32f-4daf-9fd1-7c165e5fd18a", "2f1eb4e8-38c6-4087-9449-66c453e5b361"],
+          "filters": [
+                      "a099a33a-c5da-48bc-a733-8ece97efbf8a", 
+                      "abc4fdb2-e32f-4daf-9fd1-7c165e5fd18a", 
+                      "2f1eb4e8-38c6-4087-9449-66c453e5b361"
+                    ],
           "id": "a099a33a-c5da-48bc-a733-8ece97efbf8a",
           "name": "Docking"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["4ba3aad4-447e-47c7-aa27-dea3b82a332a", "9045d03c-a683-431a-9dfd-ad69ee49a3fe", "8b5228de-7794-43cb-b6fd-386eba81af24", "1740412d-1674-4586-8cf2-0179104b4998","91433a58-4adf-484b-afe8-7724920722be", "1d1d1f41-9772-430c-bd4a-35ad385acde7"],
+          "filters": [
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a", 
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe", 
+                      "8b5228de-7794-43cb-b6fd-386eba81af24", 
+                      "1740412d-1674-4586-8cf2-0179104b4998",
+                      "91433a58-4adf-484b-afe8-7724920722be", 
+                      "1d1d1f41-9772-430c-bd4a-35ad385acde7",
+                      "6859139d-bd35-4966-a829-b002cec93e28",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6",
+                      "5b579210-b830-4a3a-902b-f382235084b2"
+                    ],
           "id": "4ba3aad4-447e-47c7-aa27-dea3b82a332a",
           "name": "Board-To-Board"
         }
@@ -94,14 +112,28 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["9045d03c-a683-431a-9dfd-ad69ee49a3fe", "4ba3aad4-447e-47c7-aa27-dea3b82a332a", "91433a58-4adf-484b-afe8-7724920722be", "1d1d1f41-9772-430c-bd4a-35ad385acde7"],
+          "filters": [
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe", 
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a", 
+                      "91433a58-4adf-484b-afe8-7724920722be", 
+                      "1d1d1f41-9772-430c-bd4a-35ad385acde7",
+                      "6859139d-bd35-4966-a829-b002cec93e28",
+                      "5b579210-b830-4a3a-902b-f382235084b2",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6"
+                    ],
           "id": "9045d03c-a683-431a-9dfd-ad69ee49a3fe",
           "name": "Cabline"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["8b5228de-7794-43cb-b6fd-386eba81af24", "4ba3aad4-447e-47c7-aa27-dea3b82a332a", "1740412d-1674-4586-8cf2-0179104b4998"],
+          "filters": [
+                      "8b5228de-7794-43cb-b6fd-386eba81af24", 
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a", 
+                      "1740412d-1674-4586-8cf2-0179104b4998",
+                      "6859139d-bd35-4966-a829-b002cec93e28",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6"
+                     ],
           "id": "8b5228de-7794-43cb-b6fd-386eba81af24",
           "name": "FPL"
         }
@@ -127,28 +159,50 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["a099a33a-c5da-48bc-a733-8ece97efbf8a","2f1eb4e8-38c6-4087-9449-66c453e5b361"],
+          "filters": [
+                      "a099a33a-c5da-48bc-a733-8ece97efbf8a",
+                      "2f1eb4e8-38c6-4087-9449-66c453e5b361"
+                     ],
           "id": "2f1eb4e8-38c6-4087-9449-66c453e5b361",
           "name": "0.175 To 1"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["1740412d-1674-4586-8cf2-0179104b4998", "8b5228de-7794-43cb-b6fd-386eba81af24", "4ba3aad4-447e-47c7-aa27-dea3b82a332a"],
+          "filters": [
+                      "1740412d-1674-4586-8cf2-0179104b4998", 
+                      "8b5228de-7794-43cb-b6fd-386eba81af24", 
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6",
+                       "6859139d-bd35-4966-a829-b002cec93e28"
+                     ],
           "id": "1740412d-1674-4586-8cf2-0179104b4998",
           "name": "1"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["91433a58-4adf-484b-afe8-7724920722be","9045d03c-a683-431a-9dfd-ad69ee49a3fe", "4ba3aad4-447e-47c7-aa27-dea3b82a332a"],
+          "filters": [
+                      "91433a58-4adf-484b-afe8-7724920722be",
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe",
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6", 
+                      "6859139d-bd35-4966-a829-b002cec93e28",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6"
+                     ],
           "id": "91433a58-4adf-484b-afe8-7724920722be",
           "name": "0.35 To 1"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["1d1d1f41-9772-430c-bd4a-35ad385acde7","9045d03c-a683-431a-9dfd-ad69ee49a3fe", "4ba3aad4-447e-47c7-aa27-dea3b82a332a"],
+          "filters": [
+                      "1d1d1f41-9772-430c-bd4a-35ad385acde7",
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe", 
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a",
+                      "5b579210-b830-4a3a-902b-f382235084b2",
+                      "6859139d-bd35-4966-a829-b002cec93e28"
+                    ],
           "id": "1d1d1f41-9772-430c-bd4a-35ad385acde7",
           "name": "0.4 To 2"
         }
@@ -164,28 +218,56 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["807778dd-0820-426b-9137-83b71782ee97","90f76566-6909-4d24-9dd6-6966e28dfddf", "fdb99586-bd29-47d4-b0b5-427d5b6a5531","36dab0bf-6fcb-475c-a551-55e3f62b70b7", "a64be78d-ae5d-4561-8018-b8bb65893f8c"],
+          "filters": [
+                      "807778dd-0820-426b-9137-83b71782ee97",
+                      "90f76566-6909-4d24-9dd6-6966e28dfddf", 
+                      "fdb99586-bd29-47d4-b0b5-427d5b6a5531",
+                      "36dab0bf-6fcb-475c-a551-55e3f62b70b7", 
+                      "a64be78d-ae5d-4561-8018-b8bb65893f8c"
+                    ],
           "id": "807778dd-0820-426b-9137-83b71782ee97",
           "name": "0.55 To 6.3"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["5b579210-b830-4a3a-902b-f382235084b2"],
+          "filters": [
+                      "5b579210-b830-4a3a-902b-f382235084b2",
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a", 
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe", 
+                      "1740412d-1674-4586-8cf2-0179104b4998",
+                      "91433a58-4adf-484b-afe8-7724920722be", 
+                      "1d1d1f41-9772-430c-bd4a-35ad385acde7",
+                      "6859139d-bd35-4966-a829-b002cec93e28"
+                    ],
           "id": "5b579210-b830-4a3a-902b-f382235084b2",
           "name": "0.85 To 2.5"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["b49c390d-135d-42e2-92ff-6a0f1d8f40bc","90f76566-6909-4d24-9dd6-6966e28dfddf", "fdb99586-bd29-47d4-b0b5-427d5b6a5531","36dab0bf-6fcb-475c-a551-55e3f62b70b7", "017aa85e-9748-415a-9ad8-3b4a3a561fb0"],
+          "filters": [
+                      "b49c390d-135d-42e2-92ff-6a0f1d8f40bc",
+                      "90f76566-6909-4d24-9dd6-6966e28dfddf", 
+                      "fdb99586-bd29-47d4-b0b5-427d5b6a5531",
+                      "36dab0bf-6fcb-475c-a551-55e3f62b70b7", 
+                      "017aa85e-9748-415a-9ad8-3b4a3a561fb0"
+                    ],
           "id": "b49c390d-135d-42e2-92ff-6a0f1d8f40bc",
           "name": "11.1 To 12.505"
         },
         {
           "active": true,
           "chosen": false,
-          "filters": ["0c1ce3f4-e384-44c6-a32d-11e5e432d0d6"],
+          "filters": [
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6",
+                      "91433a58-4adf-484b-afe8-7724920722be",
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe",
+                      "8b5228de-7794-43cb-b6fd-386eba81af24",
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a",
+                      "1740412d-1674-4586-8cf2-0179104b4998",
+                      "6859139d-bd35-4966-a829-b002cec93e28"
+                    ],
           "id": "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6",
           "name": "0.7 To 1.2"
         },
@@ -201,7 +283,14 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["a04b112e-aa4b-4843-aac2-1aaf10d8b2f8","90f76566-6909-4d24-9dd6-6966e28dfddf", "fdb99586-bd29-47d4-b0b5-427d5b6a5531", "36dab0bf-6fcb-475c-a551-55e3f62b70b7", "279de0e6-7a86-4fa6-8f20-afc6c6ca5f1a"],
+          "filters": [
+                      "a04b112e-aa4b-4843-aac2-1aaf10d8b2f8",
+                      "90f76566-6909-4d24-9dd6-6966e28dfddf", 
+                      "fdb99586-bd29-47d4-b0b5-427d5b6a5531", 
+                      "36dab0bf-6fcb-475c-a551-55e3f62b70b7", 
+                      "279de0e6-7a86-4fa6-8f20-afc6c6ca5f1a",
+                      "6859139d-bd35-4966-a829-b002cec93e28"
+                    ],
           "id": "279de0e6-7a86-4fa6-8f20-afc6c6ca5f1a",
           "name": "Horizontal"
         },
@@ -215,7 +304,17 @@ export default {
         {
           "active": true,
           "chosen": false,
-          "filters": ["6859139d-bd35-4966-a829-b002cec93e28"],
+          "filters": [
+                      "4ba3aad4-447e-47c7-aa27-dea3b82a332a", 
+                      "9045d03c-a683-431a-9dfd-ad69ee49a3fe", 
+                      "8b5228de-7794-43cb-b6fd-386eba81af24", 
+                      "1740412d-1674-4586-8cf2-0179104b4998",
+                      "91433a58-4adf-484b-afe8-7724920722be", 
+                      "1d1d1f41-9772-430c-bd4a-35ad385acde7",
+                      "5b579210-b830-4a3a-902b-f382235084b2",
+                      "6859139d-bd35-4966-a829-b002cec93e28",
+                      "0c1ce3f4-e384-44c6-a32d-11e5e432d0d6"
+                    ],
           "id": "6859139d-bd35-4966-a829-b002cec93e28",
           "name": "Right Angle"
         },
